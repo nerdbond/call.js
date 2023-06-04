3f990aa437497a0d5c5ae9b02ada4dc2b0007457
Lance Pollard <lp@elk.fm>
2023-06-04 01:42:56 -0700

    more

---
+++ test/host/call/load.ts
@@ -0,0 +1,2 @@
+const FindUserByIdTest: z.ZodType<> = z.object()
+const FindUserByNameTest: z.ZodType<> = z.object()
---
+++ test/host/form/form.ts
@@ -0,0 +1,8 @@
+export namespace Form {
+  export type Email = string
+  export type Code = string
+}
+export type Base = {
+  email: Form.Email
+  code: Form.Code
+}
